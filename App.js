1dd4c71b0cc9f2fc4740a124499aa01f3d4f463c
BrunoFerreiradaSilva <31040011+BrunoFerreiradaSilva@users.noreply.github.com>
2023-04-11 19:21:30 -0300

    frist commit done
    project base done.

---
+++ App.js
@@ -0,0 +1,19 @@
+import { SafeAreaView } from 'react-native';
+import Routes from './src/Routes';
+import { StatusBar } from 'react-native';
+import { useEffect } from 'react';
+import { criaTabela } from './src/Movies';
+
+
+export default function App() {
+  useEffect(()=> {
+    criaTabela()
+  },[])
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <StatusBar/>
+      <Routes />
+    </SafeAreaView>
+  );
+}
